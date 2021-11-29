90dd577a8d90b07bd0a9719f60cf1db336bef9de
Luciana Nascimento <luciananssantana@gmail.com>
2021-11-29 18:08:51 -0300

    simple graph TAD

---
+++ simple graph/Graph.cpp
@@ -0,0 +1,160 @@
+#include "Graph.h"
+#include "Node.h"
+#include "Edge.h"
+#include <iostream>
+#include <fstream>
+#include <stack>
+#include <queue>
+#include <list>
+#include <math.h>
+#include <cstdlib>
+#include <ctime>
+#include <float.h>
+#include <iomanip>
+
+using namespace std;
+
+/**************************************************************************************************
+ * Defining the Graph's methods
+**************************************************************************************************/
+
+// Constructor
+Graph::Graph(int order, bool directed, bool weighted_edge, bool weighted_node)
+{
+
+    this->order = order;
+    this->directed = directed;
+    this->weighted_edge = weighted_edge;
+    this->weighted_node = weighted_node;
+    this->first_node = this->last_node = nullptr;
+    this->number_edges = 0;
+}
+
+// Destructor
+Graph::~Graph()
+{
+
+    Node *next_node = this->first_node;
+
+    while (next_node != nullptr)
+    {
+
+        next_node->removeAllEdges();
+        Node *aux_node = next_node->getNextNode();
+        delete next_node;
+        next_node = aux_node;
+    }
+}
+
+// Getters
+int Graph::getOrder()
+{
+
+    return this->order;
+}
+int Graph::getNumberEdges()
+{
+
+    return this->number_edges;
+}
+//Function that verifies if the graph is directed
+bool Graph::getDirected()
+{
+
+    return this->directed;
+}
+//Function that verifies if the graph is weighted at the edges
+bool Graph::getWeightedEdge()
+{
+
+    return this->weighted_edge;
+}
+
+//Function that verifies if the graph is weighted at the nodes
+bool Graph::getWeightedNode()
+{
+
+    return this->weighted_node;
+}
+
+
+Node *Graph::getFirstNode()
+{
+
+    return this->first_node;
+}
+
+Node *Graph::getLastNode()
+{
+
+    return this->last_node;
+}
+
+// Other methods
+/*
+    The outdegree attribute of nodes is used as a counter for the number of edges in the graph.
+    This allows the correct updating of the numbers of edges in the graph being directed or not.
+*/
+void Graph::insertNode(int id)
+{
+    
+}
+
+void Graph::insertEdge(int id, int target_id, float weight)
+{
+
+    
+}
+
+void Graph::removeNode(int id){ 
+    
+}
+
+bool Graph::searchNode(int id)
+{
+    
+}
+
+Node *Graph::getNode(int id)
+{
+
+    
+}
+
+
+//Function that prints a set of edges belongs breadth tree
+
+void Graph::breadthFirstSearch(ofstream &output_file){
+    
+}
+
+
+
+float Graph::floydMarshall(int idSource, int idTarget){
+    
+}
+
+   
+
+float Graph::dijkstra(int idSource, int idTarget){
+    
+}
+
+//function that prints a topological sorting
+void topologicalSorting(){
+
+}
+
+void breadthFirstSearch(ofstream& output_file){
+
+}
+Graph* getVertexInduced(int* listIdNodes){
+
+}
+
+Graph* agmKuskal(){
+
+}
+Graph* agmPrim(){
+
+}
---
+++ simple graph/Graph.h
@@ -0,0 +1,64 @@
+/**************************************************************************************************
+ * Implementation of the TAD Graph
+**************************************************************************************************/
+
+#ifndef GRAPH_H_INCLUDED
+#define GRAPH_H_INCLUDED
+#include "Node.h"
+#include <fstream>
+#include <stack>
+#include <list>
+
+using namespace std;
+
+class Graph{
+
+    //Atributes
+    private:
+        int order;
+        int number_edges;
+        bool directed;
+        bool weighted_edge;
+        bool weighted_node;
+        Node* first_node;
+        Node* last_node;
+
+    public:
+        //Constructor
+        Graph(int order, bool directed, bool weighted_edge, bool weighted_node);
+        //Destructor
+        ~Graph();
+        //Getters
+        int getOrder();
+        int getNumberEdges();
+        bool getDirected();
+        bool getWeightedEdge();
+        bool getWeightedNode();
+        Node* getFirstNode();
+        Node* getLastNode();
+        //Other methods
+        void insertNode(int id);
+        void insertEdge(int id, int target_id, float weight);
+        void removeNode(int id);
+        bool searchNode(int id);
+        Node* getNode(int id);
+
+        //methods phase1
+        void topologicalSorting();
+        void breadthFirstSearch(ofstream& output_file);
+        Graph* getVertexInduced(int* listIdNodes);
+        Graph* agmKruskal();
+        Graph* agmPrim();
+        float floydMarshall(int idSource, int idTarget);
+        float dijkstra(int idSource, int idTarget);
+
+        //methods phase1
+        float greed();
+        float greedRandom();
+        float greedReactiveRandom();
+    private:
+        //Auxiliar methods
+
+};
+
+#endif // GRAPH_H_INCLUDED
